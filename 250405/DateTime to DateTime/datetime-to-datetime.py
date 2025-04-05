3aba762adc0f969e759ac8223b5ec7580324fe5d
Yeori <76810691+Yeonhee-Kim@users.noreply.github.com>
2025-04-05 20:37:27 +0900

    Title: DateTime to DateTime, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250405/DateTime to DateTime/datetime-to-datetime.py
+++ 250405/DateTime to DateTime/datetime-to-datetime.py
@@ -2,20 +2,21 @@ day, hour, min = 11, 11, 11
 cnt = 0
 
 a, b, c = map(int, input().split())
-
-while True:
-    if day == a and hour == b and min == c:
-        break
+if (a < 11) or (a == 11 and b < 11) or (a == 11 and b == 11 and c < 11):
+    print(-1)
+else:
+    while True:
+        if day == a and hour == b and min == c:
+            break
     
-    cnt += 1
-    min += 1
+        cnt += 1
+        min += 1
     
-    if min == 60:
-        hour += 1
-        min = 0
+        if min == 60:
+            hour += 1
+            min = 0
     
-    if hour == 24:
-        day += 1
-        hour = 0
-
-print(cnt)
+        if hour == 24:
+            day += 1
+            hour = 0
+    print(cnt)
